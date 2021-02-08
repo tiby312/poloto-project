88cf14e3386de7511b42550aa3a952ad5c3d2d77
Ken <kenakioreed@gmail.com>
2021-02-08 13:50:22 -0500

    added html example asset

--- generate_doc_graphs.sh
+++ generate_doc_graphs.sh
@@ -1,3 +1,4 @@
 mkdir assets
 cargo run --example simple > assets/simple.svg &&
 cargo run --example custom_style > assets/custom_style.svg
+cargo run --example custom_style_embed > assets/custom_style_embed.html
