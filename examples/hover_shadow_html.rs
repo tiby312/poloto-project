5acf2036a03ac71465c9ce77c013762103d3d487
Ken <kenakioreed@gmail.com>
2021-02-08 20:21:51 -0500

    added animation

---
+++ examples/hover_shadow_html.rs
@@ -0,0 +1,80 @@
+fn main() {
+    let mut s = poloto::plot("Demo: Hovering and shadows", "x", "y");
+
+    let x = (0..50).map(|x| (x as f32 / 50.0) * 10.0);
+
+    s.line("cos", x.clone().map(|x| [x, x.cos()]));
+    s.histogram("sin-3", x.clone().step_by(3).map(|x| [x, x.sin() - 3.]));
+    s.scatter("sin", x.clone().step_by(3).map(|x| [x, x.sin() ]));
+
+    println!(
+        r###"
+<html>
+{0}
+<div id="test" class="navy">
+{1}
+</div>
+</html>
+        "###,
+        HEADER,
+        s.render_to_document()
+    );
+}
+
+const HEADER: &'static str = r###"
+<head>
+<svg width=0 height=0>
+<defs>
+<filter id="dropshadow" height="130%">
+  <feGaussianBlur in="SourceAlpha" stdDeviation="3"/> <!-- stdDeviation is how much to blur -->
+  <feOffset dx="2" dy="2" result="offsetblur"/> <!-- how much to offset -->
+  <feComponentTransfer>
+    <feFuncA type="linear" slope="0.5"/> <!-- slope is the opacity of the shadow -->
+  </feComponentTransfer>
+  <feMerge> 
+    <feMergeNode/> <!-- this contains the offset blurred image -->
+    <feMergeNode in="SourceGraphic"/> <!-- this contains the element that the filter is applied to -->
+  </feMerge>
+</defs>
+</svg>
+<style>
+body {
+    background-color: coral;
+}
+@keyframes gelatine {
+    from, to { transform: scale(1, 1); }
+    25% { transform: scale(0.9, 1.1); }
+    50% { transform: scale(1.1, 0.9); }
+    75% { transform: scale(0.95, 1.05); }
+  }
+.poloto{
+    
+    text-shadow: 2px 2px 5px black;
+}
+.poloto0stroke:hover{
+    stroke:black;
+    stroke-width:3;
+}
+.poloto1fill{
+    animation: gelatine 3.0s infinite;
+    transform-origin: center;
+    filter:url(#dropshadow);
+}
+.poloto1fill:hover{
+    
+    stroke:black;
+    stroke-width:3;
+}
+
+.poloto2fill:hover{
+
+    stroke:black;
+    stroke-width:3;
+}
+.poloto2fill{
+    filter:url(#dropshadow);
+}
+
+</style>
+</head> 
+"###;
--- generate_doc_graphs.sh
+++ generate_doc_graphs.sh
@@ -3,4 +3,4 @@ cargo run --example simple > assets/simple.svg &&
 cargo run --example custom_colors_html > assets/custom_colors.html
 cargo run --example custom_style_html > assets/custom_style.html
 cargo run --example custom_style > assets/custom_style.svg
-cargo run --example hover_html > assets/hover.html
+cargo run --example hover_shadow_html > assets/hover_shadow.html
