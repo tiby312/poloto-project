f307d3d0f9bd0612d5645d295aed2bbf3fb0bbc7
Ken <kenakioreed@gmail.com>
2021-02-21 16:34:17 -0500

    added write to file_example

---
+++ examples/write_to_file.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let mut s = poloto::plot(
+        "Demo: Some Trigonometry Plots",
+        "This is the x label",
+        "This is the y label",
+    );
+
+    let x = (0..50).map(|x| (x as f32 / 50.0) * 10.0);
+
+    s.line("cos", x.clone().map(|x| [x, x.cos()]));
+
+    s.scatter("sin", x.clone().map(|x| [x, x.sin()]));
+    s.histogram("sin-10", x.clone().step_by(3).map(|x| [x, x.sin() - 10.]));
+    s.line_fill("sin-20", x.clone().map(|x| [x, x.sin() - 20.]));
+
+    let mut file=std::fs::File::create("test.svg").unwrap();
+    poloto::render_svg(tagger::upgrade(&mut file), s).unwrap();
+}
