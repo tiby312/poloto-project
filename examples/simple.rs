1ab1ba67c42686381eab80c7c43354aa1f9a958d
Ken Reed <kenakioreed@gmail.com>
2021-07-26 20:16:33 -0400

    cleanup

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,18 +2,18 @@
 fn main() {
     //Source https://en.wikipedia.org/wiki/Wikipedia:Size_of_Wikipedia
     let data = [
-        [2010,3144000],
-        [2011,3518000],
-        [2012,3835000],
-        [2013,4133000],
-        [2014,4413000],
-        [2015,4682000],
-        [2016,5045000],
-        [2017,5321200],
-        [2018,5541900],
-        [2019,5773600],
-        [2020,5989400],
-        [2021,6219700]
+        [2010, 3144000],
+        [2011, 3518000],
+        [2012, 3835000],
+        [2013, 4133000],
+        [2014, 4413000],
+        [2015, 4682000],
+        [2016, 5045000],
+        [2017, 5321200],
+        [2018, 5541900],
+        [2019, 5773600],
+        [2020, 5989400],
+        [2021, 6219700],
     ];
 
     let mut s = poloto::plot("Number of Wikipedia Articles", "Year", "Number of Articles");
@@ -23,6 +23,6 @@ fn main() {
     //Scale grpah to include up to the year 2025.
     //Also scale to include a value of 0 articles.
     s.xmarker(2025).ymarker(0.0);
-    
+
     println!("{}", s.render());
 }
