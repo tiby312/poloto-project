3931a2a4aadeadd3ee332d9737edd4d8f7d5dae3
Ken Reed <kenakioreed@gmail.com>
2021-11-19 22:47:02 -0500

    month example

---
+++ examples/month.rs
@@ -0,0 +1,26 @@
+// PIPE me to a file!
+fn main() {
+    let data = [
+        ("Jan", 3144000),
+        ("Feb", 3518000),
+        ("Mar", 3835000),
+        ("Apr", 4133000),
+        ("May", 4413000),
+        ("Jun", 4682000),
+        ("Jul", 5045000),
+        ("Aug", 5321200),
+        ("Sep", 5541900),
+        ("Oct", 5773600),
+        ("Nov", 5989400),
+        ("Dec", 6219700),
+        ("Jan 2022", 0), 
+    ];
+
+    let mut s = poloto::plot("Number of Foos in 2021", "Months of 2021", "Foos");
+
+    s.histogram("", data.iter().enumerate().map(|(c, e)| (c, e.1)));
+
+    s.xinterval_fmt(|fmt, val, _| write!(fmt, "{}", data[val.round() as usize].0));
+
+    s.simple_theme_dark(poloto::upgrade_write(std::io::stdout()));
+}
