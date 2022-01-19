5834fda4fe9fae184f3f6cf99f6c10932883b2c3
Ken Reed <kenakioreed@gmail.com>
2022-01-18 22:24:32 -0500

    better example

--- examples/years_fmt.rs
+++ examples/years_fmt.rs
@@ -4,19 +4,19 @@ use poloto::prelude::*;
 fn main() {
     //Source https://en.wikipedia.org/wiki/Wikipedia:Size_of_Wikipedia
     let data = [
-        (2010, 3144000),
-        (2011, 3518000),
-        (2012, 3835000),
-        (2013, 4133000),
-        (2014, 4413000),
-        (2015, 4682000),
-        (2016, 5045000),
-        (2017, 5321200),
-        (2018, 5541900),
-        (2019, 5773600),
-        (2020, 5989400),
-        (2021, 6219700),
-        (2022, 0), //To complete our histogram, we manually specify when 2021 ends.
+        (UnixTime::from_year(2010), 3144000),
+        (UnixTime::from_year(2011), 3518000),
+        (UnixTime::from_year(2012), 3835000),
+        (UnixTime::from_year(2013), 4133000),
+        (UnixTime::from_year(2014), 4413000),
+        (UnixTime::from_year(2015), 4682000),
+        (UnixTime::from_year(2016), 5045000),
+        (UnixTime::from_year(2017), 5321200),
+        (UnixTime::from_year(2018), 5541900),
+        (UnixTime::from_year(2019), 5773600),
+        (UnixTime::from_year(2020), 5989400),
+        (UnixTime::from_year(2021), 6219700),
+        (UnixTime::from_year(2022), 0), //To complete our histogram, we manually specify when 2021 ends.
     ];
 
     let mut s = poloto::Plotter::new(
@@ -32,12 +32,7 @@ fn main() {
         i128::ctx().no_dash().marker(0),
     );
 
-    let data = data.into_iter().map(|(a, b)| {
-        let a = UnixTime::from_year(a);
-        (a, b)
-    });
-
-    s.histogram("", data);
+    s.histogram("", &data);
 
     let mut st = String::new();
     use std::fmt::Write;
