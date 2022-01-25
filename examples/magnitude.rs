d172d8418d84b9965d08edc03ebc7cbde4e5bd34
Ken Reed <kenakioreed@gmail.com>
2022-01-24 20:58:14 -0500

    newv

--- examples/magnitude.rs
+++ examples/magnitude.rs
@@ -2,8 +2,7 @@ use poloto::prelude::*;
 fn main() {
     let data = [[0.000001, 0.000001], [0.000001000000001, 0.000001000000001]];
 
-    let mut p = poloto::plot("cows per year", "year", "cow", f64::ctx(), f64::ctx());
-    p.scatter("", &data);
+    let mut p = poloto::plot("cows per year", "year", "cow", f64::ctx(), f64::ctx()).scatter("", &data).move_into();
 
     println!("{}", poloto::disp(|a| { p.simple_theme(a) }));
 }
