21f81ffca4bdcd4a42ca24e98234e07411358ad2
Ken Reed <kenakioreed@gmail.com>
2022-01-25 20:08:56 -0500

    newv

--- examples/hours.rs
+++ examples/hours.rs
@@ -1,32 +1,32 @@
-use poloto::num::timestamp::UnixTime;
+use poloto::num::timestamp::{UnixTime, UnixTimeContext};
 use poloto::prelude::*;
 // PIPE me to a file!
 fn main() {
-    let day1 = (2020, 1, 30);
-    let day2 = (2020, 1, 31);
-    //Source https://en.wikipedia.org/wiki/Wikipedia:Size_of_Wikipedia
-    let data = [
-        (
-            UnixTime::from_ymd_hms(&chrono::Utc, day1, 12, 59, 59),
-            3144000,
-        ),
-        (UnixTime::from_ymd_hms(&chrono::Utc, day2, 1, 0, 0), 3518000),
-        (UnixTime::from_ymd_hms(&chrono::Utc, day2, 2, 2, 1), 3835000),
-        (
-            UnixTime::from_ymd_hms(&chrono::Utc, day2, 3, 59, 59),
-            2133000,
-        ),
-        (UnixTime::from_ymd_hms(&chrono::Utc, day2, 6, 1, 1), 4133000),
+    //fake hourly trend over one day.
+    let trend: [i128; 24] = [
+        0, 0, 0, 0, 0, 3, 5, 5, 10, 20, 50, 60, 70, 50, 40, 34, 34, 20, 10, 20, 10, 4, 2, 0,
     ];
 
+    let day1 = (2020, 1, 30);
+
+    let timezone = &chrono::Utc;
+
+    let data = trend
+        .into_iter()
+        .zip(0..)
+        .map(|(x, i)| (UnixTime::from_ymd_hms(timezone, day1, i, 0, 0), x));
+
     let mut s = poloto::plot(
-        "Number of Wikipedia Articles",
-        "Year",
-        "Number of Articles",
-        UnixTime::default_ctx(),
+        "Number of rides at theme park hourly",
+        "Hour",
+        "Number of rides",
+        UnixTimeContext::new(timezone).with_tick_fmt(|w, v, b, s| {
+            use chrono::Timelike;
+            write!(w, "{}", v.datetime(timezone).hour())
+        }),
         i128::default_ctx().with_marker(0),
     );
-    s.line("", &data);
+    s.histogram("", data);
 
     println!("{}", poloto::disp(|a| s.simple_theme(a)));
 }
