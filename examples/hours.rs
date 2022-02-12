d26a0fd594b043d82f7240861df556d8425cebc2
Ken Reed <kenakioreed@gmail.com>
2022-02-12 14:13:57 -0500

    better example

--- examples/hours.rs
+++ examples/hours.rs
@@ -6,16 +6,17 @@ fn main() {
         0, 0, 0, 0, 0, 3, 5, 5, 10, 20, 50, 60, 70, 50, 40, 34, 34, 20, 10, 20, 10, 4, 2, 0,
     ];
 
-    let data = trend.into_iter().zip(0..).map(|(x, i)| (i, x));
+    let data = (0..).zip(trend.into_iter());
 
-    let mut s = poloto::plot(
+    let mut plotter = poloto::plot(
         "Number of rides at theme park hourly",
         "Hour",
         "Number of rides",
         poloto::steps((0..24).step_by(5), |w, v| write!(w, "{} hr", v)),
         i128::default_ctx().with_marker(0),
     );
-    s.histogram("", data);
 
-    println!("{}", poloto::disp(|a| s.simple_theme(a)));
+    plotter.histogram("", data);
+
+    println!("{}", poloto::disp(|w| plotter.simple_theme(w)));
 }
