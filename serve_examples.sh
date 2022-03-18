3dbca5e31c5d06c887982eb430786245db3fba99
Ken Reed <kenakioreed@gmail.com>
2022-03-18 07:49:41 -0400

    build examples with release

--- serve_examples.sh
+++ serve_examples.sh
@@ -1,11 +1,11 @@
 set -ex
 
-cargo test
-cargo run --example collatz > target/assets/collatz.svg
-cargo run --example gaussian > target/assets/gaussian.svg
-cargo run --example custom_ticks > target/assets/custom_ticks.svg
-cargo run --example timestamp > target/assets/timestamp.svg
-cargo run --example bar > target/assets/bar.svg
+cargo test --release
+cargo run --release --example collatz > target/assets/collatz.svg
+cargo run --release --example gaussian > target/assets/gaussian.svg
+cargo run --release --example custom_ticks > target/assets/custom_ticks.svg
+cargo run --release --example timestamp > target/assets/timestamp.svg
+cargo run --release --example bar > target/assets/bar.svg
 
 cd target/assets
 python3 -m http.server
