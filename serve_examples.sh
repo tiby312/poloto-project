2f5505c1c78d1ded605084fc7d362f8cb62e38b1
Ken Reed <kenakioreed@gmail.com>
2022-03-23 09:34:57 -0400

    remove dev dependency

--- serve_examples.sh
+++ serve_examples.sh
@@ -1,11 +1,11 @@
 set -ex
 
-cargo test --release
+cargo test --release --features timestamp,chrono/std
 cargo run --release --example collatz > target/assets/collatz.svg
 cargo run --release --example gaussian > target/assets/gaussian.svg
 cargo run --release --example custom_ticks > target/assets/custom_ticks.svg
-cargo run --release --example timestamp > target/assets/timestamp.svg
+cargo run --release  --example timestamp > target/assets/timestamp.svg
 cargo run --release --example bar > target/assets/bar.svg
-cargo bloat  --test plots
+cargo bloat  --test plots --features timestamp,chrono/std
 cd target/assets
 python3 -m http.server
