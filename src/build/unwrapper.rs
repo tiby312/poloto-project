dc2777f8d40413d3751d830d52d1a97480153c11
Kenneth Reed <kenakioreed@gmail.com>
2022-06-15 21:28:45 -0400

    add ValRef and RefVal

--- src/build/unwrapper.rs
+++ src/build/unwrapper.rs
@@ -29,6 +29,26 @@ impl<T: PlotNum> Unwrapper for &[T; 2] {
     }
 }
 
+pub struct RefVal<'a, A, B>(pub &'a A, pub B);
+
+impl<A: PlotNum, B: PlotNum> Unwrapper for RefVal<'_, A, B> {
+    type Item = (A, B);
+    fn unwrap(self) -> (A, B) {
+        let RefVal(a, b) = self;
+        (*a, b)
+    }
+}
+
+pub struct ValRef<'a, A, B>(pub A, pub &'a B);
+
+impl<A: PlotNum, B: PlotNum> Unwrapper for ValRef<'_, A, B> {
+    type Item = (A, B);
+    fn unwrap(self) -> (A, B) {
+        let ValRef(a, b) = self;
+        (a, *b)
+    }
+}
+
 impl<A: PlotNum, B: PlotNum> Unwrapper for (A, B) {
     type Item = (A, B);
     fn unwrap(self) -> (A, B) {
