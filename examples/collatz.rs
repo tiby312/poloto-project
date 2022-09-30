cc52f936e8a2427c71d8af421f67e89c97b4ab21
Kenneth Reed <kenakioreed@gmail.com>
2022-09-30 12:52:00 -0400

    cleanup

--- examples/collatz.rs
+++ examples/collatz.rs
@@ -20,17 +20,16 @@ fn main() {
 
     let header = ps::DefaultHeader::new().with_viewbox_width(1200.0);
 
-    let plots = plots!(
-        poloto::build::plots_dyn(
-            (1000..1006)
-                .map(|i| {
-                    let name = formatm!("c({})", i);
-                    poloto::buffered_plot((0..).zip(collatz(i))).line(name)
-                })
-                .collect(),
-        ),
-        poloto::build::origin()
+    let dyn_plots = poloto::build::plots_dyn(
+        (1000..1006)
+            .map(|i| {
+                let name = formatm!("c({})", i);
+                poloto::buffered_plot((0..).zip(collatz(i))).line(name)
+            })
+            .collect(),
     );
+
+    let plots = plots!(dyn_plots, poloto::build::origin());
 
     let opt = poloto::render::render_opt_builder()
         .with_tick_lines([true, true])
