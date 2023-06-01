d4b49fc0587d743ede72fd94312d19aae322c9ce
Kenneth Reed <kenakioreed@gmail.com>
2023-06-01 17:02:51 -0400

    newv

--- poloto-evcxr/src/lib.rs
+++ poloto-evcxr/src/lib.rs
@@ -4,19 +4,22 @@ use hypermelon::{elem::Locked, prelude::Elem};
 
 
 pub fn evcxr_display<R:Elem+Locked>(elem:R){
-    let a=wrap_img_tag(elem);
-    let mut s = String::new();
-    hypermelon::render(a, &mut s).unwrap();
-    println!("EVCXR_BEGIN_CONTENT image/svg+xml\n{}EVCXR_END_CONTENT", s);
+    let a=encode(elem);
+    // let mut s = String::new();
+    // hypermelon::render(a, &mut s).unwrap();
+    println!("EVCXR_BEGIN_CONTENT image/svg+xml\n{}\nEVCXR_END_CONTENT", a);
 }
 
 
-pub fn wrap_img_tag<R:Elem+Locked>(a:R)->impl Elem+Locked{
+pub fn encode<R:Elem+Locked>(a:R)->String{
     //TODO Elem should have a render to string function?
     let mut s=String::new();
     hypermelon::render(a,&mut s).unwrap();
     use base64::Engine;
         
-    let s=format!("data:image/svg+xml;base64,{}",base64::engine::general_purpose::STANDARD.encode(&s));
-    hypermelon::build::single("img").with(("src",s))
+    //let s=format!("data:image/svg+xml;base64,{}",base64::engine::general_purpose::STANDARD.encode(&s));
+    
+    //hypermelon::build::single("img").with(("src",s))
+    base64::engine::general_purpose::STANDARD.encode(&s)
+
 }
