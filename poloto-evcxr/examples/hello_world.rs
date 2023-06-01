7b0a24d0ba48b68f5d2c0b3221ee0367107f523f
Kenneth Reed <kenakioreed@gmail.com>
2023-06-01 16:42:56 -0400

    poloto-evcxr

---
+++ poloto-evcxr/examples/hello_world.rs
@@ -0,0 +1,14 @@
+use poloto::build;
+// PIPE me to a file!
+fn main() {
+    let data = vec![[0, 0], [1, 2], [2, 3]];
+
+    let a = build::plot("label").line(data);
+
+    let v=poloto::frame_build()
+        .data(a)
+        .build_and_label(("hello world", "x", "y"))
+        .append_to(poloto::header().light_theme());
+
+    poloto_evcxr::evcxr_display(v);
+}
---
+++ poloto-evcxr/src/lib.rs
@@ -0,0 +1,22 @@
+use hypermelon::{elem::Locked, prelude::Elem};
+
+
+
+
+pub fn evcxr_display<R:Elem+Locked>(elem:R){
+    let a=wrap_img_tag(elem);
+    let mut s = String::new();
+    hypermelon::render(a, &mut s).unwrap();
+    println!("EVCXR_BEGIN_CONTENT text/html\n{}EVCXR_END_CONTENT", s);
+}
+
+
+pub fn wrap_img_tag<R:Elem+Locked>(a:R)->impl Elem+Locked{
+    //TODO Elem should have a render to string function?
+    let mut s=String::new();
+    hypermelon::render(a,&mut s).unwrap();
+    use base64::Engine;
+        
+    let s=format!("data:image/svg+xml;base64,{}",base64::engine::general_purpose::STANDARD.encode(&s));
+    hypermelon::build::single("img").with(("src",s))
+}
