de0a0a04295b0dd55b0bd4a5a8e8b651b92ee49a
Kenneth Reed <kenakioreed@gmail.com>
2023-06-01 20:12:46 -0400

    newv

---
+++ poloto-evcxr/README.md
@@ -0,0 +1,5 @@
+
+
+
+
+See the example.ipynb
--- poloto-evcxr/src/lib.rs
+++ poloto-evcxr/src/lib.rs
@@ -3,32 +3,31 @@ use hypermelon::{elem::Locked, prelude::Elem};
 
 
 
+///
+/// image/svg+xml cell with plain text
+/// 
 pub fn evcxr_display_svg<R:Elem+Locked>(elem:R){
     let mut s=String::new();
     hypermelon::render(elem,&mut s).unwrap();
     println!("EVCXR_BEGIN_CONTENT image/svg+xml\n{}\nEVCXR_END_CONTENT", s);
 }
 
+
+///
+/// html cell with image/svg+xml mime with base64 encoding
+/// 
 pub fn evcxr_display<R:Elem+Locked>(elem:R){
-    // let mut s=String::new();
-    // hypermelon::render(elem,&mut s).unwrap();
-    
-     let a=encode(elem);
-     let mut s = String::new();
-     hypermelon::render(a, &mut s).unwrap();
+    let a=encode(elem);
+    let mut s = String::new();
+    hypermelon::render(a, &mut s).unwrap();
     println!("EVCXR_BEGIN_CONTENT text/html\n{}\nEVCXR_END_CONTENT", s);
 }
 
 
 pub fn encode<R:Elem+Locked>(a:R)->impl Elem+Locked{
-    //TODO Elem should have a render to string function?
     let mut s=String::new();
     hypermelon::render(a,&mut s).unwrap();
     use base64::Engine;
-        
     let s=format!("data:image/svg+xml;base64,{}",base64::engine::general_purpose::STANDARD.encode(&s));
-    
     hypermelon::build::single("img").with(("src",s))
-    //base64::engine::general_purpose::STANDARD.encode(&s)
-
 }
