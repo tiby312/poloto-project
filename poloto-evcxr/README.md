86f8e2286692cde2e31776375b9123202e14d0cd
Kenneth Reed <kenakioreed@gmail.com>
2023-06-01 20:37:26 -0400

    newv

--- poloto-evcxr/README.md
+++ poloto-evcxr/README.md
@@ -2,5 +2,5 @@
 
 See the example.ipynb
 
-The github viewer unfortunately doesnt not support custom css for embeded svgs in a ipynb file.
+The github viewer unfortunately doesnt not support custom css for embeded svgs in a ipynb file. So this crate will inline the css.
 
--- poloto-evcxr/src/lib.rs
+++ poloto-evcxr/src/lib.rs
@@ -4,30 +4,44 @@ use hypermelon::{elem::Locked, prelude::Elem};
 
 
 ///
+/// the css is inlined so that github viewer will work
 /// image/svg+xml cell with plain text
 /// 
 pub fn evcxr_display_svg<R:Elem+Locked>(elem:R){
     let mut s=String::new();
     hypermelon::render(elem,&mut s).unwrap();
-    println!("EVCXR_BEGIN_CONTENT image/svg+xml\n{}\nEVCXR_END_CONTENT", s);
+
+    let inliner = css_inline::CSSInliner::options()
+        .inline_style_tags(true)
+        .remove_style_tags(true)
+        .build();
+    let inlined = inliner.inline(&s).unwrap();
+
+    let k=inlined.strip_prefix("<html><head></head><body>").unwrap();
+    let k=k.strip_suffix("</body></html>").unwrap();
+
+
+
+   // let inlined = css_inline::inline(&s).unwrap();
+    println!("EVCXR_BEGIN_CONTENT image/svg+xml\n{}\nEVCXR_END_CONTENT", k);
 }
 
 
-///
-/// html cell with image/svg+xml mime with base64 encoding
-/// 
-pub fn evcxr_display<R:Elem+Locked>(elem:R){
-    let a=encode(elem);
-    let mut s = String::new();
-    hypermelon::render(a, &mut s).unwrap();
-    println!("EVCXR_BEGIN_CONTENT text/html\n{}\nEVCXR_END_CONTENT", s);
-}
+// ///
+// /// html cell with image/svg+xml mime with base64 encoding
+// /// 
+// pub fn evcxr_display<R:Elem+Locked>(elem:R){
+//     let a=encode(elem);
+//     let mut s = String::new();
+//     hypermelon::render(a, &mut s).unwrap();
+//     println!("EVCXR_BEGIN_CONTENT text/html\n{}\nEVCXR_END_CONTENT", s);
+// }
 
 
-pub fn encode<R:Elem+Locked>(a:R)->impl Elem+Locked{
-    let mut s=String::new();
-    hypermelon::render(a,&mut s).unwrap();
-    use base64::Engine;
-    let s=format!("data:image/svg+xml;base64,{}",base64::engine::general_purpose::STANDARD.encode(&s));
-    hypermelon::build::single("img").with(("src",s))
-}
+// pub fn encode<R:Elem+Locked>(a:R)->impl Elem+Locked{
+//     let mut s=String::new();
+//     hypermelon::render(a,&mut s).unwrap();
+//     use base64::Engine;
+//     let s=format!("data:image/svg+xml;base64,{}",base64::engine::general_purpose::STANDARD.encode(&s));
+//     hypermelon::build::single("img").with(("src",s))
+// }
