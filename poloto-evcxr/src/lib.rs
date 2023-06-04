c769fb384eb4a5b5497f01dbac1db0827771c607
Kenneth Reed <kenakioreed@gmail.com>
2023-06-04 14:09:17 -0400

    newv

--- poloto-evcxr/src/lib.rs
+++ poloto-evcxr/src/lib.rs
@@ -31,12 +31,18 @@ pub fn evcxr_display_svg<R: Elem + Locked>(elem: R) {
 
     // //let inlined=inlined.replace("\"", "'");
 
+    use base64::Engine;
+    let s=format!("data:image/svg+xml;base64,{}",base64::engine::general_purpose::STANDARD.encode(&s));
+    let r=hypermelon::build::single("img").with(("src",s));
+    let mut s=String::new();
+    hypermelon::render(r,&mut s).unwrap();
+
 
     // let k = inlined.strip_prefix("<html><head></head><body>").unwrap();
     // let k = k.strip_suffix("</body></html>").unwrap();
 
     println!(
-        "EVCXR_BEGIN_CONTENT image/svg+xml\n{}\nEVCXR_END_CONTENT",
+        "EVCXR_BEGIN_CONTENT text/html\n{}\nEVCXR_END_CONTENT",
         s
     );
 }
